9f56aa497183473ffdbaf0f08989839bfc925a71
Shankar <thakurishankar159@gmail.com>
2026-03-15 11:42:25 +1100

    changed the port in backend

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ import { configDotenv } from "dotenv";
 configDotenv();
 
 const app = express();
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
