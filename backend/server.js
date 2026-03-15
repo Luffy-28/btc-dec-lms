9b075aa56dffa94d638c72aba3b6b38fa3317b6d
Shankar <thakurishankar159@gmail.com>
2026-03-15 11:55:00 +1100

    connected mongodb with my project

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,41 @@
 import express from "express";
 import cors from "cors";
 import { configDotenv } from "dotenv";
+import mongoose from "mongoose";
 
 configDotenv();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+const MONGO_URL = process.env.MONGO_URL || "mongodb://localhost:27017/LMS";
+
 app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("API is running");
+  return res.send({
+    status: "success",
+    message: "Financial Tracker API",
+  });
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+mongoose.connect(MONGO_URL).then(() => {});
+
+mongoose
+  .connect(MONGO_URL) // MONGOURL --> Connection string
+  .then(() => {
+    console.log("MONGO CONNECTED", MONGO_URL);
+    app.listen(PORT, (error) => {
+      if (error) {
+        console.log(error);
+        console.log("SERVER did not start!");
+      } else {
+        console.log("Server started at PORT: ", PORT);
+      }
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+    console.log("ERROR MONGO");
+  });
